0bae911cd8b44c0765d783ff76b201b8af5dcf7f
Erick Humberto Cordova Gavilanes <erihucor@espol.edu.ec>
2019-02-24 12:22:10 -0500

    Tema 3

--- FP-1P-2017-1S.py
+++ FP-1P-2017-1S.py
@@ -1,5 +1,6 @@
 #Autor Erick Humberto Cordova Gavilanes
 import numpy as np
+import random as rd
 #Tema 1
 """
 Ingrese las palabras a calificar: CAS*A*,S*ASTR*E*,R*EY*,A*ZOTE*
@@ -11,6 +12,8 @@ azote: 16
 La palabra con mayor puntaje es AZOTE (16 puntos).
 
 """
+
+print("Tema 1\n")
 L_letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 L_puntajes = [1,3,3,2,1,4,2,4,1,9,5,1,3,1,1,3,10,1,1,1,1,4,4,9,4,10]
 
@@ -38,3 +41,32 @@ for i in range(len(L_puntaje_palabra)):
     print("%s: %d"%(L_palabras_limpias[i],L_puntaje_palabra[i]))
 ind_maxima_puntuacion = L_puntaje_palabra.index(max(L_puntaje_palabra))
 print("La palabra con mayor puntaje es %s ( %d puntos )"%(L_palabras_limpias[ind_maxima_puntuacion].upper(),L_puntaje_palabra[ind_maxima_puntuacion]))
+
+#Tema 2
+print("Tema 2\n")
+
+#Tema 4
+print("Tema 4")
+"""
+a) Dada la siguiente lista L = [ 12, 9 , 1, 3, 2, 10, 20, 5, ...]
+Genere 3 posiciones aleatorias de tal manera que los valores correspondientes sumen al menos 20. Al
+final imprima la suma de los 3 valores seleccionados aleatoriamente.
+"""
+L = [ 12, 9 , 1, 3, 2, 10, 20, 5]
+suma = 0
+while not suma >= 20:
+    n_1 = L[rd.randint(0,len(L) - 1)]
+    n_2 = L[rd.randint(0,len(L) - 1)]
+    n_3 = L[rd.randint(0,len(L) - 1)]
+
+    suma = n_1 + n_2 + n_3
+print("La suma es %s"%suma)
+
+"""
+b) Dada la siguiente lista:
+¿Qué imprime el siguiente código?
+"""
+stars=['Potter', 'Ron Weasley', 'Dumbledore', 'Hermione Granger','Hagrid','Voldemort']
+
+print(stars[-4:-2])
+print( stars[3][0:stars[3].find(" ")] + stars[1][3:] )
